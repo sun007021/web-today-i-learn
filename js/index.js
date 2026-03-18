5657586d5ae01ef4a6a9d3e18e603a86fc1c8e2d
sun <sun007021@gmail.com>
2026-03-19 00:03:37 +0900

    feat: TIL 페이지 구현

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
-// TODO: TIL 폼 등록 기능을 구현하세요
-// 1. 폼 요소와 목록 요소를 querySelector로 선택합니다.
-// 2. 폼의 submit 이벤트를 감지하여 새 TIL 항목을 목록에 추가합니다.
-
 const tilForm = document.querySelector("#til-form");
 const tilList = document.querySelector("#til-list");
 
 tilForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  // TODO: 입력값을 가져와서 새 TIL 항목을 만들어 목록에 추가하세요
+  const date = document.querySelector("#til-date").value;
+  const title = document.querySelector("#til-title").value;
+  const content = document.querySelector("#til-content").value;
+
+  const article = document.createElement("article");
+  article.className = "til-item";
+
+  const time = document.createElement("time");
+  time.textContent = date;
+
+  const h3 = document.createElement("h3");
+  h3.textContent = title;
+
+  const p = document.createElement("p");
+  p.textContent = content;
+
+  article.append(time, h3, p);
+  tilList.prepend(article);
+  tilForm.reset();
 });
